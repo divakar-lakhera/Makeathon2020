f83610ba0dbcd0d9afde9aeee774db50015ff392
kanhakhatri065 <kanhakhatri065@gmail.com>
2020-02-22 17:35:20 +0530

    linkedin scraper improved

--- Makeathon2020/Makeathon2020/scrapers/linkedinScraper.py
+++ Makeathon2020/Makeathon2020/scrapers/linkedinScraper.py
@@ -1,54 +1,94 @@
+import requests, time, random
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
 
-class linkeinScraper():
+
+class linkedinScraper():
+    
     def __init__(self):
-        self.name=""
-        self.page=""
+        self.link=""
+        self.browser = webdriver.Chrome()
+        self.browser.get('https://www.linkedin.com/uas/login')
+
+        
+        time.sleep(1)
+
+        self.email = 'wajije1434@kamismail.com'
+        self.password = 'helloworld123'
+
+        elementID = self.browser.find_element_by_id('username')
+        elementID.send_keys(self.email)
+
+        elementID = self.browser.find_element_by_id('password')
+        elementID.send_keys(self.password)
+
+        time.sleep(1)
+
+        elementID.submit()
+
+        
+        
     
     def loadProfile(self, link):
-        self.name=link
-        raw_page = requests.get(link)
-        if(raw_page.status_code != 200):
-            print(f"linkediinScraper request failed : {raw_page.status_code}")
-        else :
-            self.html = BeautifulSoup(raw_page.content,'html-parser')
+        self.link=link
+        self.browser.get(link)
+
+        SCROLL_PAUSE_TIME = 2
+
+        # Get scroll height
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
+
+        for i in range(3):
+            # Scroll down to bottom
+            self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        src = self.browser.page_source
+        
+        self.soup = BeautifulSoup(src, 'lxml')
+        
+
+        
     
     def getUserName(self):
-        return self.html.find("li",attrs={"class":"inline t-24 t-black t-normal break-words"}).get_text()
+        name_div = self.soup.find('div', {'class':'flex-1 mr5'})
+        name_loc = name_div.find_all('ul')
+        name = name_loc[0].find('li').get_text().strip()
+
+        return name
     
     def getUserExperiences(self):
         experiences = []
         experience = {}
-        for exp in self.html.find_all('div', class_ = "pv-entity__summary-info pv-entity__summary-info--background-section"):
-            experience['title'] = exp.find("h3",class_="t-16 t-black t-bold").get_text()
+        for exp in self.soup.find_all('li',{"class":"pv-entity__position-group-pager pv-profile-section__list-item ember-view"}):
+            experience['title'] = exp.find("h3",{"class":"t-16 t-black t-bold"}).get_text().strip()
             
-            experience['company'] = exp.find("p", class_ = "pv-entity__secondary-title t-14 t-black t-normal").get_text()
-            
-            experience['duration'] = exp.find("div", class_ = "display-flex").find("h4",class_ = "pv-entity__date-range t-14 t-black--light t-normal").find("span").get_text()
-            
+            experience['company'] = exp.find("p", {"class":"pv-entity__secondary-title t-14 t-black t-normal"}).get_text().strip()
+                        
             experiences.append(experience)
         return experiences
     
-    def getuserEducation(self):
+    def getUserEducation(self):
         educations = []
         education = {}
 
-        edus = self.html.find("ul",attrs={'id':'ember2674',
-         'class':"pv-profile-section__section-info section-info pv-profile-section__section-info--has-no ember-view"}
-         ).find_all("div", attrs={"class":"pv-profile-section__sortable-card-item pv-education-entity pv-profile-section__card-item ember-view"})
         
-        for edu in edus:
-            education['school_name'] = edu.find("h3",class_ = "pv-entity__school-name t-16 t-black t-bold").get_text()
+        for edu in self.soup.find_all('li',{"class":"pv-profile-section__sortable-item pv-profile-section__section-info-item relative pv-profile-section__sortable-item--v2 pv-profile-section__list-item sortable-item ember-view"}):
+            education['school_name'] = edu.find("h3",{"class":"pv-entity__school-name t-16 t-black t-bold"}).get_text().strip()
             
-            education['degree_name'] = edu.find("p", class_ = "pv-entity__secondary-title pv-entity_degree-name t-14 t-black t-normal"
-            ).find("span", class_ = "pv-entity__comma-item").get_text()
+            education['degree_name'] = edu.find("p",{"class":"pv-entity__secondary-title pv-entity_degree-name t-14 t-black t-normal"}
+            ).find("span",{"class":"pv-entity__comma-item"}).get_text().strip()
 
-            education['field_of_study'] = edu.find("p", class_ = "pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"
-            ).find("span", class_ = "pv-entity__comma-item").get_text()
-            
-            dates = edu.find("p", class_ = "pv-entity__dates t-14 t-black--light t-normal").find_all("span")
-            education['dates'] = f"{dates[0]} - {dates[1]}"
+            education['field_of_study'] = edu.find("p",{"class":"pv-entity__secondary-title pv-entity__fos t-14 t-black t-normal"}
+            ).find("span",{"class":"pv-entity__comma-item"}).get_text().strip()
 
             educations.append(education)
         
@@ -58,56 +98,67 @@ class linkeinScraper():
         certifications = []
         certification = {}
 
-        certificates = self.html.find("div",attrs={'id':"ember4182", 'class':"pv-profile-section-pager ember-view"}
-        ).find_all("li",class_="pv-profile-section__sortable-item pv-certification-entity ember-view")
+        certificates = self.soup.find_all("li",{"class":"pv-profile-section__sortable-item pv-certification-entity ember-view"})
 
         for certificate in certificates:
-            certification['course_name'] = certificate.find('h3',class_ = "t-16 t-bold").get_text()
+            certification['course_name'] = certificate.find('h3',{"class":"t-16 t-bold"}).get_text().strip()
             
-            certification['company'] = certificate.find_first("p",class_ = "t-14").find("span").get_text()
-
-            life_span = str(certificate.find("p",class_ = "t-14").get_text()) + ". " + str(certification.find("p",class_ = "t-14").find("span",class_ = "pv-entity__bullet-item-v2").get_text())
-            certification['life-span'] = life_span
             certifications.append(certification)
         return certifications
     
     def getUserAccomplishments(self):
-        accomplishments = []
         accomplishment = {}
 
-        all_accomplishments = self.html.find("section",class_ = "pv-profile-section pv-accomplishments-section artdeco-container-card ember-view")
+        honors_awards = []
+        awards = self.soup.find("div",{"id":"honors-expandable-content"})
+        print(awards)
+        awards = awards.find_all("li",{"class":"pv-accomplishments-block__summary-list-item"})
+        for award in awards:
+            honors_awards.append(award.get_text().strip())
+        accomplishment['honor_awards'] = honors_awards
 
-        for i in all_accomplishments:
-            honors_awards = []
-            awards = all_accomplishments.find("div",attrs={"id":"honors-expandable-content","class":"pv-accomplishments-block__list-container"}).find_all("li", class_ = "pv-accomplishments-block__summary-list-item")
-            for award in awards:
-                honors_awards.append(award.get_text())
-            accomplishment['honor_awards'] = honors_awards
+        courses = []
+        course_list = self.soup.find("div",{"id":"courses-expandable-content",})
+        course_list = course_list.find_all("li",{"class":"pv-accomplishments-block__summary-list-item"})
+        for course in course_list:
+            courses.append(course.get_text().strip())
+        accomplishment['courses'] = courses
 
-            courses = []
-            course_list = all_accomplishments.find("div",attrs={"id":"courses-expandable-content","class":"pv-accomplishments-block__list-container"}).find_all("li",class_ = "pv-accomplishments-block__summary-list-item")
-            for course in course_list:
-                courses.append(course.get_text())
-            accomplishment['courses'] = courses
+        languages = []
+        language_list = self.soup.find("div",{"id":"languages-expandable-content"})
+        language_list = language_list.find_all("li",{"class":"pv-accomplishments-block__summary-list-item"})
+        for language in language_list:
+            languages.append(language.get_text().strip())
+        accomplishment['languages'] = languages
 
-            languages = []
-            language_list = course_list = all_accomplishments.find("div",attrs={"id":"languages-expandable-content","class":"pv-accomplishments-block__list-container"}).find_all("li",class_ = "pv-accomplishments-block__summary-list-item")
-            for language in language_list:
-                languages.append(language.get_text())
-            accomplishment['languages'] = languages
+        projects = []
+        project_list = self.soup.find("div",{"id":"projects-expandable-content"})
+        project_list = project_list.find_all("li",{"class":"pv-accomplishments-block__summary-list-item"})
+        for project in projects:
+            projects.append(project.get_text().strip())
+        accomplishments['projects'] = projects
 
-            projects = []
-            project_list = course_list = all_accomplishments.find("div",attrs={"id":"projects-expandable-content","class":"pv-accomplishments-block__list-container"}).find_all("li",class_ = "pv-accomplishments-block__summary-list-item")
-            for project in projects:
-                projects.append(project.get_text())
-            accomplishments['projects'] = projects
-
-            test_scores = []
-            test_score_list = course_list = all_accomplishments.find("div",attrs={"id":"test-scores-expandable-content","class":"pv-accomplishments-block__list-container"}).find_all("li",class_ = "pv-accomplishments-block__summary-list-item")
-            for test_score in test_score_list:
-                test_scores.append(test_score.get_text())
-            accomplishments['test_scores'] = test_scores
+        test_scores = []
+        test_score_list = course_list = self.soup.find("div",{"id":"test-scores-expandable-content"})
+        test_score_list = test_score_list.find_all("li",{"class":"pv-accomplishments-block__summary-list-item"})
+        for test_score in test_score_list:
+            test_scores.append(test_score.get_text().strip())
+        accomplishments['test_scores'] = test_scores
         
-        return accomplishments
+        return accomplishment
+    
+    def getUserSkillsEndorsements(self):
+        skills = []
+        skillsInterests = self.soup.find_all("span",{"class":"pv-skill-category-entity__name-text t-16 t-black t-bold"})
 
+        for skill in skillsInterests:
+            skills.append(skill.get_text().strip())
+        
+        return skills
+
+if __name__ == "__main__":
+    profile = linkedinScraper()
+    profile.loadProfile("https://www.linkedin.com/in/kanha-khatri-567134171/")
+
+    print(profile.getUserAccomplishments())
 
