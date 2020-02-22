c9e918f7f8bed113ab2ed54dd2bc9716fa2c16a5
Divakar Lakhera <divakarlakhera99@gmail.com>
2020-02-22 20:53:10 +0530

    Some Changes

--- Makeathon2020/Makeathon2020/views.py
+++ Makeathon2020/Makeathon2020/views.py
@@ -3,32 +3,18 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, redirect
 from Makeathon2020 import app
-from Makeathon2020.scrapers import gitScraper as gits
+from Makeathon2020.scrapers import *
 from Makeathon2020.pdfParser import parsePdf as ppar
-from Makeathon2020.scrapers import linkedinScraper as links
+
 # init
 
-newparser = ppar.pdfParser("Makeathon2020/server/test.pdf","Rahul Goswami","https://github.com/goswami-rahul",None,"https://www.linkedin.com/in/rahul101")
+newparser = ppar.pdfParser("Makeathon2020/server/test2.pdf","Pankaj Kumar","https://github.com/Pankajcoder1",None,"https://www.linkedin.com/in/pankaj-kumar-795b48198/")
 newparser.loadLinks()
 newparser.processCV()
-newparser.dumpStringPool()
-newparser.checkPool()
-"""
-mod = links.linkedinScraper()
-mod.loadProfile("https://www.linkedin.com/in/pankaj-kumar-795b48198/")
-print(mod.getUserName())
-mod.killDriver()
-newparser.dumpContent()
-"""
 
-# Pause Switch
-i=input()
-#
 """
-
-
 gitAcc=gits.gitScraper()
 gitAcc.loadProfile("https://github.com/divakar-lakhera")
 print(gitAcc.gitGetUserName())
@@ -37,3 +23,24 @@ print(gitAcc.gitGetAllProjects())
 print(gitAcc.gitGetLocation())
 # end init
 """
+
+
+@app.route("/")
+def index():
+    return render_template("file_uploading.html")
+
+@app.route("/result",methods=["POST"])
+def result():
+    name = request.form.get("name")
+    
+    linkedin_url = request.form.get("linkedin_url")
+
+    github_url = request.form.get("github_url")
+
+    stackoverflow_url = request.form.get("stackoverflow_url")
+
+    resume = request.files['resume']
+    resume.filename = "test.pdf"
+    resume.save("../server/"+resume.filename)
+
+    return render_template("file_uploading.html")
