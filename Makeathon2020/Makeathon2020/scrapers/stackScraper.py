55636d7c99ddb132ed918101a3ccbbb585e86431
Pankaj Kumar <pankajkumarmath1@gmail.com>
2020-02-22 12:15:22 +0530

    Update stackScraper.py

--- Makeathon2020/Makeathon2020/scrapers/stackScraper.py
+++ Makeathon2020/Makeathon2020/scrapers/stackScraper.py
@@ -4,37 +4,43 @@ import requests
 class stackScraper():
 	def __init__(self):
 		self.link=""
+		# list will contain name of person
 		self.name=[]
+		# list will hold all tag of that person
 		self.tags=[]
 
 	def find_tags(self,link):
-		self.link=link+"?tab=tags"
-		page_url=requests.get(link)
-		if(page_url.status_code!=200):
-			print("Stackoverflow: Request Failed... Got"+str(page_url.status_code))
+		try:
+			self.link=link+"?tab=tags"
+			page_url=requests.get(link)
+			if(page_url.status_code!=200):
+				print(f"Stackoverflow Request Failed... :{page_url.status_code}")
 
-		else:
+			else:
+				soup=BeautifulSoup(page_url.content,"html.parser")
+				raw_tag=soup.find_all("a",class_="post-tag")
+				for i in raw_tag:
+					self.tags.append(i.get_text())
 
-			soup=BeautifulSoup(page_url.content,"html.parser")
-			raw_tag=soup.find_all("a",class_="post-tag")
-			for i in raw_tag:
-				self.tags.append(i.get_text())
+				self.tags=list(set(self.tags))
+				return self.tags
 
-			self.tags=list(set(self.tags))
-			print(self.tags)
+		except:
+			return ("Invalid Url")
 
 	def find_name(self,link):
-		page_url=requests.get(link)
-		if(page_url.status_code!=200):
-			print("Stackoverflow: Request Failed... Got"+str(page_url.status_code))
+		try:
+			page_url=requests.get(self.link)
+			if(page_url.status_code!=200):
+				print("Stackoverflow: Request Failed... Got"+str(page_url.status_code))
 
-		else:
+			else:
+				soup=BeautifulSoup(page_url,"html.parser")
+				names=soup.find_all("div",class_="name")
+				for i in names:
+					i=(i.get_text()).split('\n')[1].strip()
+					self.name.append(i)
+				return name
 
-			soup=BeautifulSoup(page_url,"html.parser")
-			names=soup.find_all("div",class_="name")
-			for i in names:
-				i=(i.get_text()).split('\n')[1].strip()
-				self.name.append(i)
-
-
-
+		except:
+			return ("Invalid Url")
